0d4eacce7d9ab75f879fc55d439b205338f98dab
jsnunes <jesiel.nunes@hotmail.com>
2017-01-20 19:36:36 -0200

    Montando os projetos

---
+++ laravel-unity/app/Http/Controllers/CadastroLoginController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CadastroLoginController extends Controller
+{
+    public function cadastro()
+    {
+      # code...
+    }
+
+    public function login()
+    {
+      # code...
+    }
+}
