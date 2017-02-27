e3b5ba094b59675728e100f4ed86fdd25f189104
jsnunes <jesiel.nunes@hotmail.com>
2017-02-27 18:30:35 -0300

    Cadastro de usuário no laravel

--- laravel-unity/app/Http/Controllers/CadastroLoginController.php
+++ laravel-unity/app/Http/Controllers/CadastroLoginController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+use Validator;
+
 class CadastroLoginController extends Controller
 {
-    public function cadastro()
+    public function cadastro(Request $request)
     {
-      # code...
+      $validator = Validator::make($request->all(),[
+        'name' =>     'required',
+        'email' =>    'required|email|unique:users',
+        'password' => 'required|min:6',
+      ]);
+
+      if($validator->fails()){
+        return response()->json(['status'=>false,'errors'=>$validator->errors()->all()]);
+      }else{
+        $user = new User;
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+        $user->password = bcrypt($request->input('password'));
+        $user->save();
+        return response()->json(['status'=>true,'user'=>$user]);
+      }
     }
 
     public function login()
